421150432fe2d673e2059e8fff9135dea344f7b4
Stefan van Wouw <stefan.van.wouw@bigdatarepublic.nl>
2017-02-17 13:00:52 +0100

    Use rsync folder because of guest addition failures

--- embedded-execution-layer/vagrant.sh
+++ embedded-execution-layer/vagrant.sh
@@ -2,6 +2,6 @@
 
 # Copy the insecure (publicly known and default) vagrant private key https://github.com/mitchellh/vagrant/tree/master/keys
 # We use this key so we do not need to put a custom key authorization for docker3's key in docker1 and docker2.
-cp /vagrant/vagrant_private_key /home/vagrant/.ssh/id_rsa
+cp /vagrant/embedded-execution-layer/vagrant_private_key /home/vagrant/.ssh/id_rsa
 chmod 0400 /home/vagrant/.ssh/id_rsa
 chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
